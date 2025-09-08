682b308d1a92f2cb8e849a3da88cdf939bec95fd
Olalekan <osuolaleolalek7@gmail.com>
2025-09-08 02:02:50 -0700

    feat:route error fix

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,44 +1,45 @@
-import dbConnect from "@/lib/dbConnect"
-import { verifyUser } from "@/lib/user-action"
-import ProductModel from "@/models/Product"
+import dbConnect from "@/lib/dbConnect";
+import { verifyUser } from "@/lib/user-action";
+import ProductModel from "@/models/Product";
 // import { NextResponse } from "next/server"
 
-export const dynamic = "force-static"
+export const dynamic = "force-static";
 
 export const GET = async () => {
-  await dbConnect()
+  await dbConnect();
 
-  const products = await ProductModel.find().lean()
+  const products = await ProductModel.find().lean();
 
-  return Response.json({ products })
+  return Response.json({ products });
   //we can use Response.json or NextResponse.json({message:""})
-}
+};
 
 export const POST = async (request: Request) => {
   try {
-    await dbConnect()
+    await dbConnect();
 
-    const { title, description, price } = await request.json()
+    const { title, description, price } = await request.json();
 
-  
-    const auth = await verifyUser()
+    const auth = await verifyUser();
     if (!auth.success) {
-      return Response.json({ success: false, message: "Unauthorized" }, { status: 401 })
+      return Response.json(
+        { success: false, message: "Unauthorized" },
+        { status: 401 }
+      );
     }
 
-    
     const product = await ProductModel.create({
       title,
       description,
       price,
-      user: auth.id, 
-    })
+      user: auth.id,
+    });
 
-    return Response.json({ success: true, product }, { status: 201 })
-  } catch (error: any) {
-    console.error(error)
-    return Response.json({ success: false, message: error.message }, { status: 500 })
+    return Response.json({ success: true, product }, { status: 201 });
+  } catch (error) {
+    console.error(error);
+    return Response.json({ success: false }, { status: 500 });
   }
-}
+};
 
-export const DELETE = () => {}
+export const DELETE = () => {};
